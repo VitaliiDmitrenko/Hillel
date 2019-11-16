e6a4ee333c5b46407b47783d3162c961843210ae
Vitalii Dmitrenko <Dmitrenko.promo@gmail.com>
2019-11-16 23:33:37 +0200

    new commit

--- src/les4/Main.java
+++ src/les4/Main.java
@@ -3,6 +3,7 @@ package les4;
 public class Main {
     public static void main(String[] args) {
         int a=5, b=8;
+        int c=a+b;
         System.out.println("Hello");
         System.out.println(" ,teacher");
     }
