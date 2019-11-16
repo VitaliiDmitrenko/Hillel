2f20bf39cd915cbb1758c824f16db5495129e033
Vitalii Dmitrenko <Dmitrenko.promo@gmail.com>
2019-11-16 23:54:03 +0200

    add string

--- src/les4/Main.java
+++ src/les4/Main.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String[] args) {
         int a=5, b=8;
         int c=a+b;
+        String s;
         System.out.println("Hello");
         System.out.println(" ,teacher");
     }
